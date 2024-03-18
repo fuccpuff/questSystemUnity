f37aea70089e4b4b0feba814d459652ae986119c
hello <fuccpuff@gmail.com>
2024-03-18 13:09:21 +0500

    hz

--- Assets/Scripts/Editor/QuestCreator.cs
+++ Assets/Scripts/Editor/QuestCreator.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using UnityEditor;
 using UnityEngine;
 
@@ -5,19 +6,35 @@ public class QuestCreator
 {
     private static readonly string questResourcesPath = "Assets/Resources/Quests";
 
+    [MenuItem("Quest System/Create New Quest")]
+    public static void CreateNewQuestMenuOption()
+    {
+        string questName = "New Quest " + System.DateTime.Now.ToString("yyyyMMddHHmmss", CultureInfo.InvariantCulture);
+        CreateNewQuest(questName);
+    }
+
     public static void CreateNewQuest(string questName)
     {
-        if (!AssetDatabase.IsValidFolder($"{questResourcesPath}"))
+        if (!AssetDatabase.IsValidFolder(questResourcesPath))
         {
             AssetDatabase.CreateFolder("Assets/Resources", "Quests");
         }
 
         QuestObject newQuest = ScriptableObject.CreateInstance<QuestObject>();
+        newQuest.questId = GenerateQuestId(questName); 
+        newQuest.title = questName;
         string path = AssetDatabase.GenerateUniqueAssetPath($"{questResourcesPath}/{questName}.asset");
         AssetDatabase.CreateAsset(newQuest, path);
         AssetDatabase.SaveAssets();
 
         EditorUtility.FocusProjectWindow();
         Selection.activeObject = newQuest;
+    }
+
+    private static string GenerateQuestId(string title)
+    {
+        string date = System.DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss", CultureInfo.InvariantCulture);
+        string shortTitle = title.Length > 10 ? title.Substring(0, 10) : title;
+        return $"{shortTitle}_{date}";
     }
 }
